3bee43dbfc49d4c33f6e000c77be53ec3548e30c
Harrymanoharan, Jessey <Jessey.Harrymanoharan@amd.com>
2026-05-21 02:56:49 -0400

    [AMD][CI] Update FFM to update_ffmlite-master-20260520.152811 (#764)
    Co-authored-by: jharryma <jharryma@amd.com>

--- .github/workflows/roccap.sh
+++ .github/workflows/roccap.sh
@@ -33,8 +33,8 @@ LLVM_LIBRARY_DIR=/llvm LLVM_SYSPATH=/llvm pip3 install --no-build-isolation .
 echo "=== Setup Environment ==="
 
 source /ffm-base/ffmlite_env.sh
-#export LD_LIBRARY_PATH=/ffm-update:$LD_LIBRARY_PATH
-#export HSA_MODEL_LIB=/ffm-update/libhsakmtmodel.so
+export LD_LIBRARY_PATH=/ffm-update:$LD_LIBRARY_PATH
+export HSA_MODEL_LIB=/ffm-update/libhsakmtmodel.so
 # Prefer the NPI ROCm's libraries over the ones shipped with FFM Lite
 #export LD_LIBRARY_PATH=/opt/rocm/lib:$LD_LIBRARY_PATH
 
