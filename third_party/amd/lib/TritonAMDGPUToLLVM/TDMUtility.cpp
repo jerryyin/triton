af89ba7da57a134e59d2f9f0aedcb942251fa390
Lei Zhang <Lei.Zhang2@amd.com>
2026-06-02 01:40:00 +0000

    Revert "Rephrase TDM tensor_dim Clamping to Avoid Suboptimal Codegen (#17)"
    This reverts commit 6c4c033b4ad504445825632d47064aae63b3670b.

--- third_party/amd/lib/TritonAMDGPUToLLVM/TDMUtility.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/TDMUtility.cpp
@@ -808,10 +808,8 @@ void fillTDMDescriptor(RewriterBase &rewriter, Location loc,
   // Update tensor shapes based on offset
   for (size_t i = 0; i < numDims; ++i) {
     auto diff = b.sub(tensorShape[i], offset[i]);
-    Value tooBig = b.icmp_sgt(offset[i], tensorShape[i]);
-    Value isNeg = b.icmp_slt(offset[i], zero);
-    Value oob = b.or_(tooBig, isNeg);
-    tensorShape[i] = b.select(oob, zero, diff);
+    Value inBounds = b.icmp_ule(diff, tensorShape[i]);
+    tensorShape[i] = b.select(inBounds, diff, b.i32_val(0));
   }
 
   // TDM store does not support padding in general. However, if the padding
